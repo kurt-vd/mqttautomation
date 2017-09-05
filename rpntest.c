12ae0180d2ec92a0cb9f1c39a450f1aac46f48a7
Kurt Van Dijck <dev.kurt@vandijck-laurijssen.be>
2017-09-05 20:36:10 +0200

    rpntest: better illustrate the state of the stack at exit

--- rpntest.c
+++ rpntest.c
@@ -28,21 +28,17 @@ void rpn_run_again(void *dat)
 int main(int argc, char *argv[])
 {
 	struct rpn *rpn;
-	char *input, *fmt;
+	char *input;
+	int j;
 
 	openlog("rpntest", LOG_PERROR, LOG_LOCAL2);
 	input = argv[1];
-	fmt = strrchr(input, ' ');
-	if (fmt && fmt[1] == '%')
-		*fmt++ = 0;
 	rpn = rpn_parse(input, NULL);
 	if (!rpn)
 		return 1;
 	if (rpn_run(&rpnstack, rpn))
 		return 1;
-	if (rpnstack.n != 1)
-		printf("rpn left %u items\n", rpnstack.n);
-	printf(fmt ?: "%lf", rpnstack.v[rpnstack.n-1]);
-	printf("\n");
+	for (j = 0; j < rpnstack.n; ++j)
+		printf("[%i] %s\n", j, rpn_dtostr(rpnstack.v[j]));
 	return 0;
 }
